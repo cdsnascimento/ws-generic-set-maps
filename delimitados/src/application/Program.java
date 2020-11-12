f6af31c995495ff4b6e6e6dd0fc6a353713cc7f9
Cristiano Nascimento <cs_nascimento@yahoo.com.br>
2020-11-11 23:25:41 -0300

    204 - Delimitados (Parte 2)
    Criando um metodo genrico

--- delimitados/src/application/Program.java
+++ delimitados/src/application/Program.java
@@ -5,13 +5,17 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
+import entities.Product;
 import services.CalculationService;
 
 public class Program {
     public static void main(String[] args) {
         
-        List<Integer> list = new ArrayList<>();
+        Locale.setDefault(Locale.US);
+        
+        List<Product> list = new ArrayList<>();
 
         String path = "C:\\Users\\Note_Nascimento\\Documents\\MeuProjetos\\ws-generic-set-maps\\temp\\in.txt";
 
@@ -19,11 +23,12 @@ public class Program {
 
             String line = br.readLine();
             while (line != null){
-                list.add(Integer.parseInt(line));
+                String[] fields = line.split(",");
+                list.add(new Product(fields[0], Double.parseDouble(fields[1])));
                 line = br.readLine();
             }
 
-            Integer x = CalculationService.max(list);
+            Product x = CalculationService.max(list);
             System.out.println("Max: ");
             System.out.println(x);
 
