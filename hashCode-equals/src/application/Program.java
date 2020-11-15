5513656d7fb6b86bbe9bb3775578ae34e13ad73d
Cristiano Nascimento <cs_nascimento@yahoo.com.br>
2020-11-15 17:46:03 -0300

    207 - HashCode-Equals

---
+++ hashCode-equals/src/application/Program.java
@@ -0,0 +1,15 @@
+package application;
+
+public class Program {
+    
+    public static void main(String[] args) {
+        
+        String a = "Maria";
+        String b = "Alex";
+        System.out.println("Método Equal: " + a.equals(b));
+
+        System.out.println("Método hashCode: " + a.hashCode());
+        System.out.println("Método hashCode: " + b.hashCode());
+
+    }
+}
