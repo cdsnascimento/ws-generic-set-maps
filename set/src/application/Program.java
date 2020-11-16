33125ce0ab8f0176be9cff32c348acdd1ff3a920
Cristiano Nascimento <cs_nascimento@yahoo.com.br>
2020-11-15 22:33:45 -0300

    Set (LinkedHashSet) - Outros métodos
    - addAll(other) - união
    -retainAll(other) - interseção
    -removeAll(other) - diferença - remove do conjunto todos os elementos de other

--- set/src/application/Program.java
+++ set/src/application/Program.java
@@ -1,30 +1,34 @@
 package application;
 
-import java.util.LinkedHashSet;
 import java.util.Set;
+import java.util.TreeSet;
+import java.util.Arrays;
 
 
 public class Program {
     public static void main(String[] args) {
-        Set<String> set = new LinkedHashSet<>();
-        set.add("TV");
-        set.add("Tablet");
-        set.add("Notebook");
-        //System.out.println(set.contains("Notebook"));
-        
-        for (String p : set) {
-            System.out.println(p);
-        }
+ 
+        Set<Integer> a = new TreeSet<>(Arrays.asList(0,2,4,5,6,8,10));
+        Set<Integer> b = new TreeSet<>(Arrays.asList(5,6,7,8,9,10));
+
+        System.out.println("Conjunto A: " + a);
+        System.out.println("Conjunto B: " + b);
 
         System.out.println();
 
-        System.out.println("Removendo um item atraves do comando: .removeIf: ");
-        //set.removeIf( x -> x.length() >= 3);
-        set.removeIf( x -> x.charAt(0) == 'T');
+        //union
+        Set<Integer> c = new TreeSet<>(a);
+        c.addAll(b);
+        System.out.println("Usando o método Union: " + c);
+        //intersection
+        Set<Integer> d = new TreeSet<>(a);
+        d.retainAll(b);
+        System.out.println("Usando o método intersection: " + d);
+        //difference
+        Set<Integer> e = new TreeSet<>(a);
+        e.removeAll(b);
+        System.out.println("Usando o método difference: " + e);
 
-        for (String p : set) {
-            System.out.println(p);
-        }
 
     }
 }
