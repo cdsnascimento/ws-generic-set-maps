2f3cc7eb2f5021013840913d1cf9440232a32119
Cristiano Nascimento <cs_nascimento@yahoo.com.br>
2020-11-18 23:18:09 -0300

    213 - Map
    Demo 2
    Utilizamos HashMap, para montar a estrutura de dados.

--- map/src/application/Program.java
+++ map/src/application/Program.java
@@ -1,44 +1,28 @@
 package application;
 
+import java.util.HashMap;
 import java.util.Map;
-import java.util.TreeMap;
+
+
+import entities.Product;
 
 public class Program {
 
     public static void main(String[] args) {
         
-        Map<String, String> cookies = new TreeMap<>();
-
-        cookies.put("username", "Maria");
-        cookies.put("email", "maria@gmail.com");
-        cookies.put("phone", "9971112");
-
-        System.out.println("ALL COOKIES:");
-
-        for ( String key : cookies.keySet()){
-            System.out.println(key + ": " + cookies.get(key));
-        }
-
-        System.out.println();
-
-        cookies.remove("email");
-        cookies.put("phone", "99771133"); // como o Map não aceita repetições o valor inserido, sera sobrescrito ao original
-
-        System.out.println("BEFORE REMOVE EMAIL");
-
-        for ( String key : cookies.keySet()){
-            System.out.println(key + ": " + cookies.get(key));
-        }
-
-        System.out.println();
-        System.out.println("-----------");
-        System.out.println();
-
-        System.out.println("Contains 'phone' key: " + cookies.containsKey("phone"));
-        System.out.println("Phone numbre: " + cookies.get("phone")); // trazer o valor contido na chave email
-
-        System.out.println("Email: " + cookies.get("email")); // como nao exite a chave email o valor de retorno será nulo
-        System.out.println("Size: " + cookies.size()); // retorna o total de valores contidos na estrutura
+        Map<Product, Double> stock = new HashMap<>();
+        
+        Product p1 = new Product("Tv", 900.0);
+        Product p2 = new Product("Notebook", 1200.0);
+        Product p3 = new Product("Tablet", 400.0);
+        
+        stock.put(p1, 10000.0);
+        stock.put(p2, 20000.0);
+        stock.put(p3, 15000.0);
+        
+        Product ps = new Product("Tv", 900.0);
+        
+        System.out.println("Contains 'ps' key: " + stock.containsKey(ps));
 
     }
     
