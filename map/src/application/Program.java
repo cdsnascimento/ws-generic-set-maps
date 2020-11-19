d1058cd9b45fb0d16da16a4c44cb4ede2ee1c427
Cristiano Nascimento <cs_nascimento@yahoo.com.br>
2020-11-18 23:08:43 -0300

    213 - Map (remove, contains, get  e size)
    Demo 1
    Alguns teste para os principais métodos

--- map/src/application/Program.java
+++ map/src/application/Program.java
@@ -22,12 +22,23 @@ public class Program {
         System.out.println();
 
         cookies.remove("email");
+        cookies.put("phone", "99771133"); // como o Map não aceita repetições o valor inserido, sera sobrescrito ao original
 
         System.out.println("BEFORE REMOVE EMAIL");
 
         for ( String key : cookies.keySet()){
             System.out.println(key + ": " + cookies.get(key));
         }
+
+        System.out.println();
+        System.out.println("-----------");
+        System.out.println();
+
+        System.out.println("Contains 'phone' key: " + cookies.containsKey("phone"));
+        System.out.println("Phone numbre: " + cookies.get("phone")); // trazer o valor contido na chave email
+
+        System.out.println("Email: " + cookies.get("email")); // como nao exite a chave email o valor de retorno será nulo
+        System.out.println("Size: " + cookies.size()); // retorna o total de valores contidos na estrutura
 
     }
     
